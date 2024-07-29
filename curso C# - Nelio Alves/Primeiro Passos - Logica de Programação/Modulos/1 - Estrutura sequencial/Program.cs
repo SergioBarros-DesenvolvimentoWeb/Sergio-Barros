e5c44b15e57ff37b43d2b7d071d4860a89e353ce
Sergio Barros | Software/Game Development <100400431+SergioBarros-DesenvolvimentoWeb@users.noreply.github.com>
2024-07-29 18:13:54 -0300

    Add files via upload

---
+++ curso C# - Nelio Alves/Primeiro Passos - Logica de Programação/Modulos/1 - Estrutura sequencial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estrutura Sequencial \n Dia 1");
+            Console.WriteLine("\n Variaveis e seus tipos");
+            Console.WriteLine("string -> guarda valores como textos");
+            Console.WriteLine("char -> guarda valores como letra inicial.. geralmente usada em definições de sexo (M/F) ou confirmações (S/N - Y/N)");
+            Console.WriteLine("int = guarda valores de numeros inteiros");
+            Console.WriteLine("float = guarda valores de numeros com virgula");
+            Console.WriteLine("bool = guarda valores como verdadeito ou falto (false/true)");
+
+
+        }
+    }
+}
